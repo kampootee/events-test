0692452c7a62551b447cf899aae2424ae43f629e
kampootee <xkarmaamputee@gmail.com>
2026-05-06 18:42:52 +0100

    Update config.js

--- config.js
+++ config.js
@@ -6,8 +6,8 @@ window.FIREBASE_CONFIG = {
     messagingSenderId: "364858297232",
     appId: "1:364858297232:web:f442ed40221fd0f6e8c115",
     measurementId: "G-JHRW8HD7K1"
-    window.ADMIN_UID = "FMv14irUYzes4glPHCNV8BexIqK2";
+
 };
 3.  **Create a `.gitignore` file** and add `config.js` to it.
 
-
+    window.ADMIN_UID = "FMv14irUYzes4glPHCNV8BexIqK2";
