4122321bcb0b26ddb5333324fc47b34cbfccddc5
kampootee <xkarmaamputee@gmail.com>
2026-05-06 17:48:46 +0100

    Create config.js

---
+++ config.js
@@ -0,0 +1,10 @@
+window.FIREBASE_CONFIG = {
+    apiKey: "AIzaSyDAUR74Lfb2RYae0qDj9FLK4YdX4Cf-qic",
+    authDomain: "localevents-2f7e4.firebaseapp.com",
+    projectId: "localevents-2f7e4",
+    storageBucket: "localevents-2f7e4.firebasestorage.app",
+    messagingSenderId: "364858297232",
+    appId: "1:364858297232:web:f442ed40221fd0f6e8c115",
+    measurementId: "G-JHRW8HD7K1"
+};
+3.  **Create a `.gitignore` file** and add `config.js` to it.
